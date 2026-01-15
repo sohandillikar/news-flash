f8f7b9091005d7cbb95ea7dd5d8c80805c65e260
Sohan Dillikar <sohanyt.main@gmail.com>
2026-01-14 19:26:45 -0800

    created express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.get('/health', (req, res) => {
+  res.json({ Hello: 'World' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
